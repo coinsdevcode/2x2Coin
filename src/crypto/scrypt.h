0cb112e856a4307ad1058bcc989117f04d3a287f
coindev <webmastercase20@gmail.com>
2026-04-25 11:29:36 -0300

    scrytp-N update 105.000 block
    Update SCRYPT_SCRATCHPAD_SIZE and function signatures

--- src/crypto/scrypt.h
+++ src/crypto/scrypt.h
@@ -3,19 +3,25 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-// N=4096
-static const int SCRYPT_SCRATCHPAD_SIZE = 131072 + 63;
+static const int SCRYPT_SCRATCHPAD_SIZE = 262144 + 63;
 
-// Scrypt-N
-void scrypt_1024_1_1_256(const char *input, char *output, unsigned int N);
-
-// generic
-void scrypt_N_1_1_256_sp_generic(const char *input, char *output, char *scratchpad, unsigned int N);
+void scrypt_1024_1_1_256(const char *input, char *output, int nHeight);
+void scrypt_1024_1_1_256_sp_generic(const char *input, char *output, char *scratchpad);
+void scrypt_ASIC_RESISTANT_sp_generic(const char *input, char *output, char *scratchpad);
 
 #if defined(USE_SSE2)
-    void scrypt_detect_sse2();
-    void scrypt_1024_1_1_256_sp_sse2(const char *input, char *output, char *scratchpad);
-    extern void (*scrypt_1024_1_1_256_sp_detected)(const char *input, char *output, char *scratchpad);
+#if defined(_M_X64) || defined(__x86_64__) || defined(_M_AMD64) || (defined(MAC_OSX) && defined(__i386__))
+#define USE_SSE2_ALWAYS 1
+#define scrypt_1024_1_1_256_sp(input, output, scratchpad) scrypt_1024_1_1_256_sp_sse2((input), (output), (scratchpad))
+#else
+#define scrypt_1024_1_1_256_sp(input, output, scratchpad) scrypt_1024_1_1_256_sp_detected((input), (output), (scratchpad))
+#endif
+
+void scrypt_detect_sse2();
+void scrypt_1024_1_1_256_sp_sse2(const char *input, char *output, char *scratchpad);
+extern void (*scrypt_1024_1_1_256_sp_detected)(const char *input, char *output, char *scratchpad);
+#else
+#define scrypt_1024_1_1_256_sp(input, output, scratchpad) scrypt_1024_1_1_256_sp_generic((input), (output), (scratchpad))
 #endif
 
 void
